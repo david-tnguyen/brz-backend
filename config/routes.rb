aa3d91bfada476691ae39e74cc5bbdbdc582b194
David Nguyen <tnguyen.david@gmail.com>
2021-09-11 22:45:50 -0700

    set up auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :yahoo_quotes, only: [:index, :show]
   resources :yahoo_price, only: [:show]
   resources :tiingo_search, only: [:show]
